817421e23bb4bf019ec8f3e1858a219155540a3d
JackBoyd927 <56743564+JackBoyd927@users.noreply.github.com>
2021-03-18 12:14:12 -0400

    Add files via upload

---
+++ ClassExamples/src/com/asc/oop/main/RandomExampleApp.java
@@ -0,0 +1,29 @@
+package com.asc.oop.main;
+
+import java.util.Scanner;
+
+public class RandomExampleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String inpt = "";
+		String fName = "";
+		String lName = "";
+				
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the program \nPlease enter your name in the following format (first last)");
+		
+		inpt = sc.nextLine();
+		
+		// Jack Boyd
+		fName = inpt.substring(0,4);
+		System.out.println("First name is " + fName);
+		
+		if()
+		{
+			
+		}
+	}
+
+}
